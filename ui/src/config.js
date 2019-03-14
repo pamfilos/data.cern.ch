35a80ff844fda50e1dfda5219603c8c0e5954b33
Pamfilos Fokianos <pamfilosf@gmail.com>
2019-03-14 12:00:39 +0100

    check gitlabci
    Signed-off-by: Pamfilos Fokianos <pamfilosf@gmail.com>

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,7 +1,7 @@
 const config = {
-  "project": {
-    "name": "CERN Analysis Preservation",
-    "api": "https://pammbp.cern.ch:5000"
+  project: {
+    name: "CERN CERN Analysis Preservation",
+    api: "https://pammbp.cern.ch:5000"
   }
 };
 
