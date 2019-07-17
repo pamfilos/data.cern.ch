66018498c63889eb4b7c2378b6cfaff31796858c
Pamfilos Fokianos <pamfilosf@gmail.com>
2019-07-17 14:21:02 +0200

    deploy: updates script for auto-deploy
    Signed-off-by: Pamfilos Fokianos <pamfilosf@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,11 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then 
     echo "Triggers build for PR:  ${TRAVIS_PULL_REQUEST_SLUG}";
     echo "UPSTREAM REPO: ${TRAVIS_REPO_SLUG} PR: ${TRAVIS_PULL_REQUEST}";
+    echo "PR AGAINST ${TRAVIS_BRANCH}";
+    repo_owner="$(cut -d'/' -f1 <<<"$TRAVIS_PULL_REQUEST_SLUG")"
+
     if ! [ -z "$GITLAB_PIPELINE_TRIGGER_TOKEN" ]; then
-        ./scripts/build-and-deploy.sh -p ${TRAVIS_PULL_REQUEST};
+        ./scripts/build-and-deploy.sh -p ${TRAVIS_PULL_REQUEST}; -d ${repo_owner}
     fi
 fi
 
